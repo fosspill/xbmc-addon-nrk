1ba2076ccc8b8227bcb9896f1c0d1de969bb3445
fosspill <b9ac4a2f-45b5-4551-9fe8-2083f56bc2f2@anonaddy.me>
2020-09-07 10:10:48 +0200

    Update subs.py

--- subs.py
+++ subs.py
@@ -25,7 +25,7 @@ from io import BytesIO
 
 def get_subtitles(video_id):
     media_element_url = "http://psapi-granitt-prod-we.cloudapp.net/mediaelement/%s" % (video_id)
-    media_element_json = requests.get('media_element_url')
+    media_element_json = requests.get(media_element_url)
     ttml_sub_url = media_element_json.json()["subtitlesUrlPath"]
     print ttml_sub_url
     html = requests.get(ttml_sub_url).text
