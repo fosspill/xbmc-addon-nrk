c31e9fc901a502599389abc0ec2cec75cee4e475
fosspill <b9ac4a2f-45b5-4551-9fe8-2083f56bc2f2@anonaddy.me>
2020-09-07 09:55:30 +0200

    Update subs.py

--- subs.py
+++ subs.py
@@ -25,7 +25,7 @@ from io import BytesIO
 def get_subtitles(video_id):
     vtt_sub_url = "https://undertekst.nrk.no/prod/%s/%s/%s/TTV/%s.vtt" % (video_id[0:6], video_id[6:8], video_id, video_id)
     ttml_sub_url = "https://undertekst.nrk.no/prod/%s/%s/%s/TMP/%s.ttml" % (video_id[0:6], video_id[6:8], video_id, video_id)
-    html = requests.get(vtt_sub_url).text
+    html = requests.get(ttml_sub_url).text
     if not html:
         return None
 
